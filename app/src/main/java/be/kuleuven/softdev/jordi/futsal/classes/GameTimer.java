c60e126afa44dc3eb8ef712967a245d8ae8d7b01
jordi <jordi.huyse@student.kuleuven.be>
2018-04-28 11:12:30 +0200

    Added activities that should be closer to end version. Added a few classes to model the match. Added two todos, however there is way more to do.

---
+++ app/src/main/java/be/kuleuven/softdev/jordi/futsal/classes/GameTimer.java
@@ -0,0 +1,26 @@
+package be.kuleuven.softdev.jordi.futsal.classes;
+
+
+
+public class GameTimer {
+    private long time;
+
+    public GameTimer() {
+        time = 0;
+    }
+
+    public long getTimeStamp(){
+        return time;
+    }
+
+    public String getTimeString()
+    {
+        StringBuilder string = new StringBuilder();
+        string.append(time);
+        string.insert(0,":");
+        string.insert(0,time);
+
+        return string.toString();
+    }
+
+}
